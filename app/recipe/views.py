035216dbf4cdb147f0245e78013f28009c614e42
Aniketh Babu <aniketh@aniketh.local>
2023-10-27 13:47:51 -0400

    fixed linting errors

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -48,4 +48,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-    
